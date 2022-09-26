19df0e7ce70388726cb6bc42c5fd028d2ee6d5cc
Rasmus Wallberg <rasmus.grey@hotmail.se>
2022-09-26 15:40:32 +0200

    Visual genes tier

--- Components/Hero/HeroBackAppendageCell.jsx
+++ Components/Hero/HeroBackAppendageCell.jsx
@@ -2,8 +2,29 @@ import React from "react";
 
 export default function HeroBackAppendageCell({ hero }) {
   return (
-      <>
-        {hero.backAppendage}
-      </>
+    <>
+      {
+        {
+          0: "Basic1",
+          1: "Basic2",
+          2: "Basic3",
+          3: "Basic4",
+          4: "Basic5",
+          5: "Basic6",
+          6: "Basic7",
+          7: "Basic8",
+          8: "Basic9",
+          9: "Basic10",
+          16: "Advanced1",
+          17: "Advanced2",
+          18: "Advanced3",
+          19: "Advanced4",
+          20: "Advanced5",
+          24: "Elite1",
+          25: "Elite2",
+          28: "Transcendent1",
+        }[hero.backAppendage]
+      }
+    </>
   );
 }
--- Components/Hero/HeroHairCell.jsx
+++ Components/Hero/HeroHairCell.jsx
@@ -3,7 +3,27 @@ import React from "react";
 export default function HeroHairCell({ hero }) {
   return (
       <>
-        {hero.hairStyle}
+        {
+        {
+          0: "Basic1",
+          1: "Basic2",
+          2: "Basic3",
+          3: "Basic4",
+          4: "Basic5",
+          5: "Basic6",
+          6: "Basic7",
+          7: "Basic8",
+          8: "Basic9",
+          9: "Basic10",
+          16: "Advanced1",
+          17: "Advanced2",
+          18: "Advanced3",
+          19: "Advanced4",
+          20: "Advanced5",
+          24: "Elite1",
+          25: "Elite2",
+          28: "Transcendent1",
+        }[hero.hairStyle]}
       </>
   );
 }
